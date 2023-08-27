8c6151a5e8b9bc8705ae8bfc2b54e1733b25ede6
Thomas Hartmann <40410024+ThomasHartmannDev@users.noreply.github.com>
2023-08-28 00:24:56 +0200

    Fixed the problem that if a letter was inserted in the place of a number, the program would break and end its execution.

--- src/main/java/ch/th/Main.java
+++ src/main/java/ch/th/Main.java
@@ -1,5 +1,6 @@
 package ch.th;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -12,30 +13,37 @@ public class Main {
         TicketMachine ticketMachine = new TicketMachine();
         ticketMachine.reset();
         boolean check = false;
+        int choice;
         while (!check){
-            System.out.println("+----------------------------+");
-            System.out.println("|       Ticket Automat       |");
-            System.out.println("+----------------------+-----+");
-            System.out.println("|  Zone                |  1  |");
-            System.out.println("|  Ticket Class        |  2  |");
-            System.out.println("|  Pay                 |  3  |");
-            System.out.println("|  Close               |  0  |");
-            System.out.println("+----------------------+-----+");
-            System.out.println("Chose the action: ");
-            int choice = scanner.nextInt();
+           try {
+               System.out.println("+----------------------------+");
+               System.out.println("|       Ticket Automat       |");
+               System.out.println("+----------------------+-----+");
+               System.out.println("|  Zone                |  1  |");
+               System.out.println("|  Ticket Class        |  2  |");
+               System.out.println("|  Pay                 |  3  |");
+               System.out.println("|  Close               |  0  |");
+               System.out.println("+----------------------+-----+");
+               System.out.println("Chose the action: ");
 
-            switch (choice){
-                case 1: ticketMachine.zoneIDSetter(); System.out.println(ticketMachine); break;
-                case 2: ticketMachine.ticketSetter(); System.out.println(ticketMachine); break;
-                case 3: if(ticketMachine.pay()){
-                            System.out.println(ticketMachine);
-                            ticketMachine.reset();
-                        }
-                        System.out.println(ticketMachine);
-                        break;
-                case 0: check = true; break;
-                default: System.out.println("Inserted wrong Value"); break;
-            }
+               choice = scanner.nextInt();
+
+               switch (choice){
+                   case 1: ticketMachine.zoneIDSetter(); System.out.println(ticketMachine); break;
+                   case 2: ticketMachine.ticketSetter(); System.out.println(ticketMachine); break;
+                   case 3: if(ticketMachine.pay()){
+                       System.out.println(ticketMachine);
+                       ticketMachine.reset();
+                   }
+                       System.out.println(ticketMachine);
+                       break;
+                   case 0: check = true; break;
+                   default: System.out.println("Inserted wrong Value"); break;
+               }
+           } catch (InputMismatchException e){
+               System.out.println("Invalid input. Please enter a valid number.");
+               scanner.next();
+           }
         }
 
         System.out.println(ticketMachine);
--- src/main/java/ch/th/TicketMachine.java
+++ src/main/java/ch/th/TicketMachine.java
@@ -1,6 +1,7 @@
 package ch.th;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,11 +11,11 @@ public class TicketMachine {
     private int zoneID;
     private boolean halfTicket;
     private int ticketClass;
-    private double zonePrice;
-    private int firstClassPrice;
-    private int secondClassPrice;
+    private final double zonePrice;
+    private final int firstClassPrice;
+    private final int secondClassPrice;
 
-    List<Double> coins = new ArrayList<>();
+    List<Double> money = new ArrayList<>();
     Scanner scannerIn = new Scanner(System.in);
 
     TicketMachine(){
@@ -28,9 +29,10 @@ public class TicketMachine {
         this.secondClassPrice = 1;
         this.zonePrice = 3;
 
-        this.coins = new ArrayList<>(List.of(0.10,0.20,0.50,1.00,2.00,5.00));
+        this.money = new ArrayList<>(List.of(0.10,0.20,0.50,1.00,2.00,5.00, 10.00));
     }
     public void reset() {
+        total = 0;
         balance = 0;
         zoneID = 0;
         halfTicket = false;
@@ -38,39 +40,68 @@ public class TicketMachine {
     }
 
     public void insertCoin(double value) {
-        if(coins.contains(value)){
+        if(money.contains(value)){
             balance = balance + value;
         } else {
-            System.out.printf("Not accepted coin... %.2f throwed back",value);
+            System.out.printf("Not accepted money... %.2f throwed back\n",value);
         }
 
     }
 
+    public int getZoneID() {
+        return zoneID;
+    }
+
+    public String isHalfTicket() {
+        if(halfTicket){
+            return "Yes";
+        }
+        return "No";
+    }
+
+    public int getTicketClass() {
+        return ticketClass;
+    }
+
     public void zoneIDSetter(){
-        System.out.println("Write the Zone Number (1000-9999): ");
-        int postID = scannerIn.nextInt();
+        int postID;
         int confirm = 0;
         boolean check = false;
-        while(confirm != 1){
-            while(!check){
-                if(postID >= 1000 && postID <= 9999 ){
-                    System.out.printf("You selected the zone: %d \n", postID);
-                    this.zoneID = postID;
-                    check = true;
-                } else if(postID != 1) {
-                    System.out.println("Wrong value inserted!");
+        while(confirm != 1) {
+            while (!check) {
+                try {
                     System.out.println("Write the Zone Number (1000-9999): ");
                     postID = scannerIn.nextInt();
-                } else if(postID == 1){
-                    System.out.println("Write the New Zone Number (1000-9999): ");
-                    postID = scannerIn.nextInt();
-                }
+                    if (postID >= 1000 && postID <= 9999) {
+                        System.out.printf("You selected the zone: %d \n", postID);
+                        this.zoneID = postID;
+                        check = true;
+                    } else if (postID != 1) {
+                        System.out.println("Wrong value inserted!");
+                        System.out.println("Write the Zone Number (1000-9999): ");
+                        postID = scannerIn.nextInt();
+                    } else if (postID == 1) {
+                        System.out.println("Write the New Zone Number (1000-9999): ");
+                        postID = scannerIn.nextInt();
+                    }
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid number.");
+                    scannerIn.next();
             }
-            System.out.println("Is the zone Right?\nYes - 1\nNo - 2");
-            confirm = scannerIn.nextInt();
-            if(confirm == 2){
-                postID = 1;
-                check = false;
+        }
+            try{
+                System.out.println("Is the zone Right?\nYes - 1\nNo - 2");
+                confirm = scannerIn.nextInt();
+                if(confirm == 2){
+                    postID = 1;
+                    check = false;
+                } else if(confirm != 2 && confirm != 1){
+                    System.out.println("Inserted wrong value\n");
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Invalid input. Please enter a valid number.");
+                scannerIn.next();
             }
         }
 
@@ -81,62 +112,70 @@ public class TicketMachine {
         boolean checkHalfTicket = false;
         int confirmTicketClass = 2;
         while (!checkTicket) {
-            System.out.println("+----------------------------+");
-            System.out.println("|     Select your ticket     |");
-            System.out.println("+----------------------+-----+");
-            System.out.println("|  First Class         |  1  |");
-            System.out.println("|  Second Class        |  2  |");
-            System.out.println("+----------------------+-----+");
-            System.out.println("Your Ticker: ");
-            int ticketClass = scannerIn.nextInt();
+            try{
+                System.out.println("+----------------------------+");
+                System.out.println("|     Select your ticket     |");
+                System.out.println("+----------------------+-----+");
+                System.out.println("|  First Class         |  1  |");
+                System.out.println("|  Second Class        |  2  |");
+                System.out.println("+----------------------+-----+");
+                System.out.println("Your Ticker: ");
+                int ticketClass = scannerIn.nextInt();
 
-            switch (ticketClass) {
-                case 1:
-                    System.out.println("You chose the First Class");
-                    this.ticketClass = 1;
-                    checkTicket = true;
-                    break;
-                case 2:
-                    System.out.println("You chose the Second Class");
-                    this.ticketClass = 2;
-                    checkTicket = true;
-                    break;
-                default:
-                    System.out.println("Wrong value inserted!");
-                    break;
+                switch (ticketClass) {
+                    case 1:
+                        System.out.println("You chose the First Class");
+                        this.ticketClass = 1;
+                        checkTicket = true;
+                        break;
+                    case 2:
+                        System.out.println("You chose the Second Class");
+                        this.ticketClass = 2;
+                        checkTicket = true;
+                        break;
+                    default:
+                        System.out.println("Wrong value inserted!");
+                        break;
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Invalid input. Please enter a valid number.");
+                scannerIn.next();
             }
         }
-
         while (!checkHalfTicket){
+            try{
 
-            System.out.println("+----------------------------+");
-            System.out.println("|         HalfTicket         |");
-            System.out.println("+----------------------+-----+");
-            System.out.println("|  Yes                 |  1  |");
-            System.out.println("|  No                  |  2  |");
-            System.out.println("+----------------------+-----+");
-            int ticketClass = scannerIn.nextInt();
 
-            switch (ticketClass) {
-                case 1:
-                    System.out.println("You chose Half ticket");
-                    this.halfTicket = true;
-                    checkHalfTicket = true;
-                    break;
-                case 2:
-                    System.out.println("You chose the Normal ticket");
-                    this.halfTicket = false;
-                    checkHalfTicket = true;
-                    break;
+                System.out.println("+----------------------------+");
+                System.out.println("|         HalfTicket         |");
+                System.out.println("+----------------------+-----+");
+                System.out.println("|  Yes                 |  1  |");
+                System.out.println("|  No                  |  2  |");
+                System.out.println("+----------------------+-----+");
+                int ticketClass = scannerIn.nextInt();
 
-                default:
-                    System.out.println("Wrong value inserted!");
-                    break;
+                switch (ticketClass) {
+                    case 1:
+                        System.out.println("You chose Half ticket");
+                        this.halfTicket = true;
+                        checkHalfTicket = true;
+                        break;
+                    case 2:
+                        System.out.println("You chose the Normal ticket");
+                        this.halfTicket = false;
+                        checkHalfTicket = true;
+                        break;
+
+                    default:
+                        System.out.println("Wrong value inserted!");
+                        break;
+                }
+
+            } catch (InputMismatchException e){
+                System.out.println("Invalid input. Please enter a valid number.");
+                scannerIn.next();
             }
         }
-
-
-
     }
 
     private boolean payValidation(){
@@ -168,10 +207,15 @@ public class TicketMachine {
             }
 
             while(balance < total){
-                System.out.println("Insert a coin: ");
-                double insertedCoin = scannerIn.nextDouble();
-                insertCoin(insertedCoin);
-                System.out.printf("Balance: %.2f\n", balance);
+                try{
+                    System.out.println("Insert a coin: ");
+                    double insertedCoin = scannerIn.nextDouble();
+                    insertCoin(insertedCoin);
+                    System.out.printf("Balance: %.2f\n", balance);
+                } catch (InputMismatchException e){
+                    System.out.println("Invalid input. Please enter a valid number.");
+                    scannerIn.next();
+                }
             }
             if(balance > total){
                 double value = balance - total;
@@ -184,7 +228,6 @@ public class TicketMachine {
 
         //return false;
     }
-
     @Override
     public String toString() {
         return "TicketMachine{" +
